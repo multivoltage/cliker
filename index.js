29a7061f0b758e3f9171a6acd775c4be3e772498
diego tonini <diego.tonini.ext@yoox.com>
2018-03-14 00:26:42 +0100

    add setup.j

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ console.dir(u);// return external IPv4
 console.dir(myIP('IPv6'));// return external IPv6 
 console.dir(myIP(null,true));// return internal IPv4 
 console.dir(myIP('IPv6',true));
+
+var TorControl = require('tor-control');
+ 
+var control = new TorControl({
+    password: 'password',                     // Your password for tor-control
+    persistent: false                         // Keep connection (persistent)
+});
+ 
+control.signalNewnym(function (err, status) { // Get a new circuit
+   if (err) {
+      return console.error(err);
+   }
+   console.log(status.messages[0]); // --> "OK"
+});
+ 
+control.getInfo(['version', 'exit-policy/ipv4'], function (err, status) { // Get info like describe in chapter 3.9 in tor-control specs.
+   if (err) {
+      return console.error(err);
+   }
+   console.log(status.messages.join(' - '));
+});
---
+++ tor/setup.js
@@ -0,0 +1,83 @@
+/** 
+ * here we check we have some command and some file and download some file :)
+*/
+var shell = require('shelljs');
+var fs = require('fs');
+
+const VERSION = '7.5.1';
+const TOR_BROWSER_WGET_URL = 'https://www.torproject.org/dist/torbrowser/'+VERSION+'/tor-browser-linux64-'+VERSION+'_en-US.tar.xz';
+const filename_tor_browser = 'tor-browser-linux64-'+VERSION+'_en-US.tar.xz';
+
+function init(){
+  if(!isAllToolsAvailable())
+    return;
+
+  if(!isTorBrowserDownloaded())
+    return;
+
+  if(!isTorCorrectlyExtracted())
+    return;
+
+}
+
+function isTorBrowserDownloaded(){
+  // Run external tool synchronously
+  console.log('try to download tor browser from '+TOR_BROWSER_WGET_URL);
+  // we run get and save file inside ./tor folder of this project
+  if (shell.exec('wget '+TOR_BROWSER_WGET_URL+' tor').code !== 0) {
+    console.log('cannot download tor browser correctly. Check connection or valid url');
+    shell.exit(1);
+    return false;
+  }
+  return true;
+}
+
+function isAllToolsAvailable(){
+  if (!shell.which('sh')) {
+    shell.echo('please install sh before run this app');
+    shell.exit(1);
+    return false;
+  }
+  if (!shell.which('nc')) {
+    shell.echo('please install nc before run this app');
+    shell.exit(1);
+    return false;
+  }
+  if (!shell.which('curl')) {
+    shell.echo('please install curl before run this app');
+    shell.exit(1);
+    return false;
+  }
+  if (!shell.which('grep')) {
+    shell.echo('please install grep before run this app');
+    shell.exit(1);
+    return false;
+  }
+  return true;
+}
+
+function isTorCorrectlyExtracted(path){
+  console.log('I am extracting tor browser into folder');
+  if (shell.exec('tar xf tor/'+filename_tor_browser).code !== 0) {
+    console.log('cannot execute tar xf of tor/'+filename_tor_browser);
+    shell.exit(1);
+    return false;
+  }
+  console.log('Deleting file...');
+  fs.unlink('tor/'+filename_tor_browser,function(){
+    console.log('correctly deleted file');
+    return true;
+  });
+}
+
+function givePermissionForShScript(){
+  console.log('try to give permission to sh/*.sh scripts');
+  if (shell.exec('chmod 755 ./tor/sh/*.sh').code !== 0) {
+    console.log('cannot execute chmod 755 '+filename_tor_browser);
+    shell.exit(1);
+    return false;
+  }
+  return true;
+}
+
+init();
