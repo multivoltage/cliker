7cc8a26a853a0c2ebd4583bc69f69c7f73fdfd0f
diego tonini <diego.tonini.ext@yoox.com>
2018-03-13 23:27:14 +0100

    chromium:headless

--- runner.js
+++ runner.js
@@ -1,12 +1,13 @@
 const createTestCafe = require('testcafe');
 let runner           = null;
 
+console.log('runner.js start :)');
 createTestCafe('localhost', 1337, 1338)
     .then(testcafe => {
         runner = testcafe.createRunner();
         runner
             .src('tests/adfly.js')
-            .browsers('chrome')
+            .browsers('chrome:headless')
             .run()
             .then(failedCount => {
                 /* ... */
@@ -18,12 +19,15 @@ createTestCafe('localhost', 1337, 1338)
                 /* ... */
                 console.log(error);
                 testcafe.close();
-                process.exit(failedCount ? 1 : 0);
+                process.exit(error ? 1 : 0);
             });
-                    
+
         return testcafe.createBrowserConnection();
     })
     .then(remoteConnection => {
          /* ... */
-        
+        console.log('return remoteConnection')
+        console.log(remoteConnection);
+        testcafe.close();
+        process.exit(0);
     });
